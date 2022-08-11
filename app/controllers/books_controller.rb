f42094d16dfb08c4c53ba58f0c59eb2e3d5dfcab
ChiakiTakayama <106967678+ChiakiTakayama@users.noreply.github.com>
2022-08-11 02:25:13 +0000

    フラッシュメッセージ追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,6 +11,7 @@ def create
  @book.user_id = current_user.id
   if @book.save
    redirect_to book_path(@book.id)
+   flash[:success]="You have created book successfully."
   else
    @books = Book.all
    @user = current_user 
@@ -42,6 +43,7 @@ def update
   book = Book.find(params[:id])
  if book.update(book_params)
   redirect_to book_path(book.id)
+  flash[:success]="You have updated book successfully."
  else
   render :edit
  end
