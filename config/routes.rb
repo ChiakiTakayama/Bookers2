5871ce7d173892a13d2db88a8bdf6d33ea00f3f8
ChiakiTakayama <106967678+ChiakiTakayama@users.noreply.github.com>
2022-08-21 07:39:13 +0000

    books/editのシンタックスエラー解決

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
 root to: "homes#top"
+
 devise_for :users
 resources :books,only: [:new, :create, :edit, :update, :index, :show, :destroy]
 resources :users,only: [:show, :edit, :update, :index]#13章で追加、:createも必要？
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-get "/homes/about" => "homes#about", as: "about"
+get "/home/about" => "homes#about", as: "about"
 end
